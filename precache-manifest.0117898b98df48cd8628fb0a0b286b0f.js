93c8ee3ed55294eab035dc16144d6bed7922f7eb
Orsoev Mikhail <plainliving@protonmail.com>
2020-10-21 12:54:39 +0800

    Updates

---
+++ precache-manifest.0117898b98df48cd8628fb0a0b286b0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c69feb608c491a43e055297e38b3de3",
+    "url": "/reactKanbanBoard/index.html"
+  },
+  {
+    "revision": "fec3b08776b463b38151",
+    "url": "/reactKanbanBoard/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "31e4f14c2a798b35c9be",
+    "url": "/reactKanbanBoard/static/css/main.822ed849.chunk.css"
+  },
+  {
+    "revision": "fec3b08776b463b38151",
+    "url": "/reactKanbanBoard/static/js/2.5aa6773a.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/reactKanbanBoard/static/js/2.5aa6773a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e4f14c2a798b35c9be",
+    "url": "/reactKanbanBoard/static/js/main.7ec210ec.chunk.js"
+  },
+  {
+    "revision": "8c09681906ee5329be18",
+    "url": "/reactKanbanBoard/static/js/runtime-main.845b16e0.js"
+  }
+]);
