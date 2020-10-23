94982f72800d510a7f1fe57bfb5c74a2a6e83817
Orsoev Mikhail <n0tEternal@ya.ru>
2020-10-24 00:07:51 +0800

    Updates

--- precache-manifest.8ed4f9e7a8ed6bc2f07b7787242c50db.js
+++ precache-manifest.8ed4f9e7a8ed6bc2f07b7787242c50db.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "53f2e9101e4e137f06c8978466f9cf17",
+    "revision": "e0e2d853b30e2d4ef83002195031c913",
     "url": "/reactKanbanBoard/index.html"
   },
   {
-    "revision": "267d1264958f84d6398e",
+    "revision": "47f807c12f506e34dabe",
     "url": "/reactKanbanBoard/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "81bb32e99211c595dddd",
+    "revision": "52a6c349a0606754e7d0",
     "url": "/reactKanbanBoard/static/css/main.db0206fa.chunk.css"
   },
   {
-    "revision": "267d1264958f84d6398e",
-    "url": "/reactKanbanBoard/static/js/2.81dab8ff.chunk.js"
+    "revision": "47f807c12f506e34dabe",
+    "url": "/reactKanbanBoard/static/js/2.5fb372d8.chunk.js"
   },
   {
     "revision": "8d190e2d02f8e768921a63f56a741895",
-    "url": "/reactKanbanBoard/static/js/2.81dab8ff.chunk.js.LICENSE.txt"
+    "url": "/reactKanbanBoard/static/js/2.5fb372d8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "81bb32e99211c595dddd",
-    "url": "/reactKanbanBoard/static/js/main.fc02d160.chunk.js"
+    "revision": "52a6c349a0606754e7d0",
+    "url": "/reactKanbanBoard/static/js/main.4d09c87f.chunk.js"
   },
   {
     "revision": "8c09681906ee5329be18",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactKanbanBoard/precache-manifest.7131502bc471205b3be95194d6d1e2ce.js"
+  "/reactKanbanBoard/precache-manifest.8ed4f9e7a8ed6bc2f07b7787242c50db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactKanbanBoard/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
